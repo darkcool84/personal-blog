cb60a00f1492ed5d173ef6d24f59b4bec0de454f
HWPC0324\HWPC0324 <jskim01@namuga.co.kr>
2026-07-29 09:35:26 +0900

    Redesign blog home page

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = '개인 개발 블로그';
-export const SITE_DESCRIPTION = 'Astro, React, MDX로 만든 개인 기술 블로그';
+export const SITE_DESCRIPTION = '개발 기술 분석, 게임·만화·책·캠핑 같은 취미 생활, 주식과 기업 분석을 기록하는 개인 블로그';
